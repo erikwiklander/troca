67b79612daac4cdbaa270ef5751a1b15660f6ce6
Erik Wiklander <erik.wiklander@str8lines.com>
2022-10-10 18:32:51 -0500

    Initial commit

---
+++ src/main/java/org/wiklander/troca/TrocaApplication.java
@@ -0,0 +1,13 @@
+package org.wiklander.troca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrocaApplication.class, args);
+	}
+
+}
